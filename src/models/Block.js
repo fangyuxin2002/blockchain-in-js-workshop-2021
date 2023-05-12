20ada2fad25ff6d2118617fac1c2fcf7ac6a18d1
didshow <116046091+didshow@users.noreply.github.com>
2023-05-12 21:11:43 +0800

    Update Block.js

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,9 +1,29 @@
+import SHA256 from  '../../node_modules/crypto-js/sha256.js'
+
+// Block
 class Block {
   // 1. 完成构造函数及其参数
   /* 构造函数需要包含
-
+      - 时间戳
+      - 交易列表
+      - 前一个区块的哈希值
+      - 当前区块的哈希值
   */
-  constructor() {}
+  constructor(timestamp, transactions, previousHash = '') {
+    this.timestamp = timestamp
+    this.transactions = transactions
+    this.previousHash = previousHash
+    this.hash = this.calculateHash()
+  }
+
+  // 2. 定义 calculateHash 函数
+  /* 
+    计算当前区块的哈希值
+  */
+  calculateHash() {
+    return SHA256(this.timestamp + JSON.stringify(this.transactions) + this.previousHash).toString()
+  }
 }
 
 export default Block
+
