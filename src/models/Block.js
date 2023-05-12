1bbdbdcbbac4e0ff18bcf75671240dbe05df565b
didshow <116046091+didshow@users.noreply.github.com>
2023-05-12 22:25:11 +0800

    Update Block.js

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,21 +1,22 @@
 import SHA256 from  '../../node_modules/crypto-js/sha256.js'
+import Blockchain from './Blockchain.js'
 
 // Block
 class Block {
   // 1. 完成构造函数及其参数
   /* 构造函数需要包含
-      - 区块链
+      - 区块链的链对象
       - 前序hash
       - index
-      - 时间戳
+      - 本区块的哈希值
   */
-  constructor(blockchain,previousHash="",index, timestamp) {
-    blockchain,
+  constructor(blockchain,previousHash,index, timestamp) {
+    this.blockchain=blockchain
+    this.previousHash=previousHash
     this.index=index,
-    this.timestamp=SHA256(new Date().getTime().toString()).toString()
+    this.timestamp=timestamp
+    blockchain.addBlock(this)
   }
 }
 
 export default Block
-
-
