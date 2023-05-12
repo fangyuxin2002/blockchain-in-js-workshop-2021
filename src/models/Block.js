a3c6622a22a0087bf1bd2ee441fb55dfb6e8c077
didshow <116046091+didshow@users.noreply.github.com>
2023-05-12 21:27:43 +0800

    Update Block.js

--- src/models/Block.js
+++ src/models/Block.js
@@ -4,26 +4,18 @@ import SHA256 from  '../../node_modules/crypto-js/sha256.js'
 class Block {
   // 1. 完成构造函数及其参数
   /* 构造函数需要包含
+      - 区块链
+      - 前序hash
+      - index
       - 时间戳
-      - 交易列表
-      - 前一个区块的哈希值
-      - 当前区块的哈希值
   */
-  constructor(timestamp, transactions, previousHash = '') {
-    this.timestamp = timestamp
-    this.transactions = transactions
-    this.previousHash = previousHash
-    this.hash = this.calculateHash()
-  }
-
-  // 2. 定义 calculateHash 函数
-  /* 
-    计算当前区块的哈希值
-  */
-  calculateHash() {
-    return SHA256(this.timestamp + JSON.stringify(this.transactions) + this.previousHash).toString()
+  constructor(blockchain,previousHash="",index, timestamp) {
+    blockchain,
+    this.index=index,
+    this.timestamp=SHA256(new Date().getTime().toString()).toString()
   }
 }
 
 export default Block
+
 
