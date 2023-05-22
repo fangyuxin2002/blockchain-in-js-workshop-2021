ec789d7e316cb66ab98626c2441fa107c9991393
ben ben <1349769271@qq.com>
2023-05-22 09:45:43 +0800

    实验三完成

--- src/models/UTXOPool.js
+++ src/models/UTXOPool.js
@@ -1,16 +1,32 @@
 import UTXO from './UTXO.js'
 
 class UTXOPool {
-  constructor(utxos = {}) {}
+  constructor(utxos=[]) {
+    this.utxos=utxos
+
+  }
 
   // 添加交易函数
   /**
    * 将交易的信息更新至 UTXOPool 中
    */
-  addUTXO() {}
+  addUTXO(input,output) {
+
+    if (!this.utxos[output]){
+      this.utxos[output]=new UTXO(input,output,12.5)}
+    else {
+      this.utxos[output] = new UTXO(input, output, this.utxos[output].amount + 12.5)
+    }
+  }
 
   // 将当前 UXTO 的副本克隆
-  clone() {}
+  clone() {
+    let res=[]
+    for (let item in this.utxos){
+      res[item]=this.utxos[item]
+    }
+    return res
+  }
 }
 
 export default UTXOPool
