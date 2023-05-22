080db396d282a94b0033cbe1651f71eababc99d3
ben ben <1349769271@qq.com>
2023-05-22 10:08:50 +0800

    实验三完成，并完成注释

--- src/models/UTXO.js
+++ src/models/UTXO.js
@@ -1,5 +1,5 @@
 export default class UTXO {
-  constructor(input,output,amount) {
+  constructor(input,output,amount) {//记录:输入，输出，然后金额(当然输入输出还是会有金额的，这里为了简单一点，就把地址当成输入输出)
     this.input=input
     this.output=output
     this.amount=amount
