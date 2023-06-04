53aeeb1c4c0a8a62dd5279606dea342715a23b41
ben ben <1349769271@qq.com>
2023-06-04 11:10:54 +0800

    实验四完成，并完成注释

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -2,8 +2,11 @@ import sha256 from 'crypto-js/sha256.js'
 
 
 class Transaction {
-  constructor() {
-    
+  constructor(transactionIn,transactionOut,price) {
+    this.transactionIn=transactionIn
+    this.transactionOut=transactionOut
+    this.price=price
+    this.hash=this._calculateHash()
   }
 
   // 更新交易 hash
@@ -13,7 +16,7 @@ class Transaction {
 
   // 计算交易 hash 的摘要函数
   _calculateHash() {
-   
+    return  sha256(this.transactionIn+this.transactionOut+this.price).toString()
   }
 }
 
